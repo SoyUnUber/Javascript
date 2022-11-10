7cf6a94ed780b2fd981a398cfcea5e8867ec8bc8
Noname <102451761+SoyUnUber@users.noreply.github.com>
2022-11-10 12:20:47 -0300

    Create 5-naipes-rework.cs

---
+++ 5-naipes-rework.cs
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<style>
+    body {
+        background: #292929;
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        height: 100vh;   
+    }
+
+    .cartabox {
+        perspective: 500px;
+     margin: 15px;
+     
+
+    }
+    .carta{
+        position: relative;
+        width: 250px;
+        height: 390px;
+        background: #fff;
+        transform-style: preserve-3d;
+        transition: .5s ease;
+        box-shadow: 0 30px 30px rgba(0, 0, 0, .5);
+    }
+
+    .cartafrontal , .cartatrasera {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 150px;
+        height: 150px;
+        color: #fff;
+        backface-visibility: hidden;
+
+    }
+
+    .cartafrontal
+    {
+        font-size: 40px;
+        justify-content: center;
+        text-align: center;
+
+    }
+
+    .cartabox:hover .carta
+    {
+        transform: rotateY(180deg);
+    }
+
+.cartatrasera
+{
+    transform: rotateY(180deg);
+}
+    
+    
+</style>
+<body>
+ 
+<div class="cartabox">
+    <div class="carta">
+        <div class="cartafrontal"></div>
+        <div class="cartatrasera">cartaa</div>
+    </div>
+</div>
+
+<div class="cartabox">
+    <div class="carta">
+        <div class="cartafrontal"></div>
+        <div class="cartatrasera">cartaa</div>
+    </div>
+</div>
+
+<div class="cartabox">
+    <div class="carta">
+        <div class="cartafrontal"></div>
+        <div class="cartatrasera">carta</div>
+    </div>
+</div>
+
+
+</body>
+</html>
